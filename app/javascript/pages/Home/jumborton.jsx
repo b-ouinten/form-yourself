5cd0e9dc6d2e3852ef3d8e64063a5fdf42493190
Nazaninf24 <nazaninf24@gmail.com>
2020-12-03 13:55:01 +0100

    add

--- app/javascript/pages/Home/jumborton.jsx
+++ app/javascript/pages/Home/jumborton.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
+import bgimage from '../../../assets/images/image_background.jpg'
 
 const Jumborton = ()  => (
 
-		<div className="jumbotron">
-			<h1 className="display-3">Hello, world!</h1>
+	// <Jumbotron style={{ backgroundImage: `url(${bgimage})`, backgroundSize: 'cover' }}>
+		<div className="jumbotron text-center">
+			<h3 className="display-5">Form-Yourself</h3>
 			<p className="lead">This is a simple hero unit, a simple jumbotron-style component for calling extra attention to featured content or information.</p>
 			<div className="my-4">
 			<p>It uses utility classNamees for typography and spacing to space content out within the larger container.</p>
@@ -12,7 +14,7 @@ const Jumborton = ()  => (
 			</p>
 			</div>
 		</div>
-		
+		// </Jumbotron>
 )
 
 export default Jumborton
